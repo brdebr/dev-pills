523c44d7fc82b130694cb9c3e385e79753ab9f5f
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2020-08-04 13:50:49 +0200

    Add placeholder theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,17 +66,17 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
+        light: {
+          primary: colors.teal.base,
+          secondary: colors.pink.base,
+          accent: colors.indigo.base,
+          error: colors.red.base,
           warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
+          info: colors.lightBlue.base,
+          success: colors.lightGreen.base
+        }
       },
     },
   },
