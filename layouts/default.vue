a25635e85f43174b7492b9364e1fa1f1117d4113
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2020-08-05 00:17:23 +0200

    Basic app-bar collapse

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,10 +4,17 @@
       v-model="drawer"
       :mini-variant="miniVariant"
       mini-variant-width="80"
-      class="the-drawer"
+      :class="`the-drawer ${collapse ? 'the-drawer--collapsed' : null}`"
       clipped
       app
     >
+      <v-list v-if="collapse && !miniVariant">
+        <v-list-item class="text-center headline">
+          <span>
+            Dev - Pills
+          </span>
+        </v-list-item>
+      </v-list>
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -31,6 +38,17 @@
           <v-btn
             outlined
             :block="!miniVariant"
+            @click.stop="collapse = !collapse"
+          >
+            <v-icon>
+              mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}
+            </v-icon>
+          </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn
+            outlined
+            :block="!miniVariant"
             @click.stop="miniVariant = !miniVariant"
           >
             <v-icon>
@@ -40,15 +58,31 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar clipped-left app>
+    <v-app-bar
+      clipped-left
+      app
+      outlined
+      flat
+      :collapse="collapse"
+      class="the-app-bar"
+    >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>
         Dev - Pills
       </v-toolbar-title>
       <v-spacer />
     </v-app-bar>
-    <v-main>
-      <v-container class="fill-height" style="background-color: #fefcf9;">
+    <v-main
+      :style="{
+        backgroundColor: '#fefcf9',
+        paddingRight: collapse ? '120px' : null,
+      }"
+      :class="collapse ? 'pt-1' : null"
+    >
+      <v-container
+        :class="`fill-height ${collapse ? 'pr-3' : null}`"
+        style="background-color: #fefcf9;"
+      >
         <nuxt />
       </v-container>
     </v-main>
@@ -66,6 +100,7 @@ export default {
     return {
       drawer: false,
       miniVariant: false,
+      collapse: false,
       items: [
         {
           icon: 'mdi-apps',
@@ -85,9 +120,20 @@ export default {
 
 <style lang="scss">
 .the-drawer {
+  &--collapsed {
+    top: 0 !important;
+    max-height: calc(100% - 36px) !important;
+  }
   .v-navigation-drawer__content {
     display: flex;
     flex-direction: column;
+  }
+}
+.the-app-bar {
+  margin-left: auto;
+  &.v-toolbar--collapsed {
+    border-bottom-right-radius: 0px !important;
+    border-bottom-left-radius: 24px !important;
   }
 }
 </style>
