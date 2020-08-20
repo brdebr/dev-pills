be00289127dce15f573f3744b315c732cdfeff2e
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2020-08-20 21:47:18 +0200

    Make footer borders notched

--- components/Layout/TheFooter.vue
+++ components/Layout/TheFooter.vue
@@ -3,10 +3,12 @@
     <div class="fixed-footer__content" :style="containerStyles">
       <v-footer
         :class="
-          'px-6 pb-5 pt-7 mx-auto rounded-t-xl toolbar-color footer-bar ' +
+          'px-6 pb-5 pt-7 mx-auto rounded-t-xl footer-bar cut-shape ' +
           ($vuetify.breakpoint.xsOnly ? 'footer-bar--mobile' : '')
         "
         max-width="75%"
+        :dark="!$vuetify.theme.dark"
+        :light="$vuetify.theme.dark"
         absolute
       >
         <div
@@ -115,17 +117,29 @@ export default class TheFooter extends Vue {
       bottom: -110px !important;
     }
     transition: bottom 0.15s ease-in-out;
-    border: 2px solid;
-    border-bottom: 0;
-    &.theme--light {
-      border-color: #002922 !important;
-    }
-    &.theme--dark {
-      border-color: #d0ebe4 !important;
-    }
     &:hover {
       bottom: 0px !important;
     }
+    &.theme--dark {
+      background-color: #029789 !important;
+    }
+    &.theme--light {
+      background-color: #d0ebe4 !important;
+    }
   }
+}
+
+.cut-shape {
+  --topXDiffSize: 25px;
+  --topYDiffSize: 20px;
+
+  clip-path: polygon(
+    var(--topXDiffSize) 0%,
+    calc(100% - var(--topXDiffSize)) 0%,
+    100% var(--topYDiffSize),
+    100% 100%,
+    0 100%,
+    0% var(--topYDiffSize)
+  );
 }
 </style>
