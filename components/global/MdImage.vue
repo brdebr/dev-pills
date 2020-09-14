bf37a0fe7247ccad162959bde19f79a9b547d750
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2020-09-14 22:01:41 +0200

    Add image component to use inside .md

---
+++ components/global/MdImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-img :src="imgSrc()" :lazy-src="imgSrc()" :alt="alt" :title="alt" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    imgSrc() {
+      try {
+        return require(`~/assets/images${this.src}`)
+      } catch (error) {
+        return null
+      }
+    },
+  },
+}
+</script>
